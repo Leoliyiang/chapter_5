cf35708f6ed4e77f55f5dc4371006588ef30add7
Leoliyiang <31967637+Leoliyiang@users.noreply.github.com>
2017-10-25 08:36:17 -0400

    practise

---
+++ Leo1.py
@@ -0,0 +1,11 @@
+res=input("What's your name")
+
+if res=="Leo"or res=="Maikel":
+    print("Group 2")
+
+elif res=="Kayla"or res=="Genesis":
+        print("Group 4")
+elif res=="Justis H"or res=="David":
+        print("Group 3")
+else:
+    print("I don't know the group.")    
---
+++ Leo2.py
@@ -0,0 +1,13 @@
+res=float(input("What's your age"))
+if res<4:
+    print("not in school")
+elif res>=4 and res<5:
+    print("Pre School")
+elif res>=5 and res<10:
+    print("Lower School")
+elif res>=10 and res<13:
+    print("Middle School")
+elif res>=13 and res<18:
+    print("Upper School")
+elif res>=18:
+    print("not in school")
